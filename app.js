2be0353ef21d4c8369fe1297ddd79e18ffc3b556
rkique <ericpingxia@gmail.com>
2021-01-31 12:39:04 -0800

    add start script (node app.js)

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
+var fs = require('fs')
 var http = require('http');
-var fs = require('fs');
-var mongo = require('mongodb')
+var MongoClient = require('mongodb').MongoClient;
 
-var MongoClient = mongo.MongoClient;
-var url = "mongodb://localhost:27017/mydb";
 
-MongoClient.connect(url, function(err, db) {
+
+var obj = JSON.parse(fs.readFileSync('terms.json', 'utf8'));
+
+console.log(obj.terms[0].key1)
+
+
+http.createServer(function(req,res){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(obj.terms[0].key1);
+}).listen(3000, function () {console.log("server started port 3000")})
+
+
+
+
+
+//var url = "mongodb://localhost:27017/";
+
+/*MongoClient.connect(url, function(err, db) {
+
   if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
-
-http.createServer(function (req, res) {
-  fs.readFile('terms.json', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
+  var dbo = db.db("mydb");
+  var myobj = { short: "Plasmid", definition: "A small circular fragment of DNA"};
+  dbo.collection("customers").insertOne(myobj, function(err,res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
   });
-}).listen(8080);    
+});*/
